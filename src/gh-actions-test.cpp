45193fe747e9a5c659267b0da624907ede3f48f9
John Gibbon <john.gibbon@comspace.de>
2020-09-30 09:59:45 +0200

    resume successful build

--- src/gh-actions-test.cpp
+++ src/gh-actions-test.cpp
@@ -15,6 +15,6 @@ int main(int argc, char *argv[])
     //   - SailfishApp::pathToMainQml() to get a QUrl to the main QML file
     //
     // To display the view, call "show()" (will show fullscreen on device).
-    failtest;
+
     return SailfishApp::main(argc, argv);
 }
